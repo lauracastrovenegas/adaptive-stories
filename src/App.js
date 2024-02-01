5e1972c7f1b0dc76342fb0af330baa36fe9e55ad
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2024-02-01 15:09:12 -0800

    initialized react app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const PageWrapper = styled.div`
+  padding: 2rem;
+`;
+
+const App = () => {
+  return (
+    <PageWrapper>
+      Out Site
+    </PageWrapper>
+  );
+}
+
+export default App;
