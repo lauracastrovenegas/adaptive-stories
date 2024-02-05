321140fe2e8fe89287ee11c7add2004dd15a0d52
Ellie Yang <elliezy@stanford.edu>
2024-02-04 19:20:02 -0800

    horizontal timeline initial changes

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import HorizontalTimeline from './horizontalTimeline/HorizontalTimeline';
 
 const TimelineWrapper = styled.div`
-  border: 1px solid black;
   padding 2rem;
 `;
 
+/*
+Format: YYYY-MM-DD
+Note: Make sure dates are sorted in increasing order
+*/
+const VALUES = [
+  '2008-06-01',
+  '2010-06-01',
+  '2013-06-01',
+  '2015-03-01',
+  '2019-01-01',
+  '2019-06-17',
+  '2019-08-01',
+];
+
 const Timeline = () => {
+  const [value, setValue] = useState(0);
+  // const [previous, setPrevious] = useState(0);
   return (
-    <TimelineWrapper>This is the timeline feature</TimelineWrapper>
+      <TimelineWrapper>
+        {/* Bounding box for the Timeline */}
+        <div style={{ width: '100%', height: '6rem', margin: '1rem auto' }}>
+          <HorizontalTimeline
+            index={value}
+            indexClick={(index) => {
+              // setPrevious(value)
+              setValue(index);
+              // console.log('hi')
+              // this.setState({ value: index, previous: this.state.value });
+            }}
+            values={ VALUES } />
+        </div>
+        <div className='text-center'>
+          {/* any arbitrary component can go here */}
+          {value}
+        </div>
+      </TimelineWrapper>
   );
 };
 
